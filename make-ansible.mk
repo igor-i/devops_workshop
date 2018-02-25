712e25042a17091a506ea43305ae19b2cd04a103
Igor Inkovskiy <Inkovskiy@gmail.com>
2018-02-25 17:30:11 +0300

    step 37

--- make-ansible.mk
+++ make-ansible.mk
@@ -5,3 +5,6 @@ ansible-vaults-encrypt:
 ansible-vaults-decrypt:
 	ansible-vault decrypt ansible/development/group_vars/all/vault.yml --ask-vault-pass
 	ansible-vault decrypt ansible/group_vars/all/vault.yml --ask-vault-pass
+
+ansible-deps-install:
+	ansible-galaxy install -r requirements.yml
